cd4d94fda1ff2eee0ae691f7ca94d827716f2ca5
Gonzalo <gonsp24@gmail.com>
2025-07-24 17:14:31 -0300

    registro modelos reservas y usuarios

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('rol',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('rol',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
